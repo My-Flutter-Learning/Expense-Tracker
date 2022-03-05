1e71b69628e76a076b8514fb371679b13b6f2237
victormutuku <victor.mutuku@strathmore.edu>
2022-03-05 10:55:10 +0300

    Added modal bottom sheet

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+import 'package:expense_tracker/widgets/new_transaction.dart';
 import 'package:flutter/material.dart';
 
-import './widgets/user_transactions.dart';
+import './widgets/transaction_list.dart';
+import './models/transaction.dart';
 
 void main() => runApp(const MyApp());
 
@@ -9,21 +11,60 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: 'Expense Tracker',
       home: MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  MyHomePage({Key? key}) : super(key: key);
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
 
-  // String titleInput = "";
-  // String amountInput = "";
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
 
-  final titleController = TextEditingController();
-  final amountController = TextEditingController();
+class _MyHomePageState extends State<MyHomePage> {
+  
+  final List<Transaction> _userTransactions = [
+    Transaction(
+      id: 't1',
+      title: 'New Shoes',
+      amount: 69.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Weekly Groceries',
+      amount: 34.56,
+      date: DateTime.now(),
+    )
+  ];
+
+  void _addNewTransaction(String title, double amount) {
+    final newTx = Transaction(
+        id: DateTime.now().toString(),
+        title: title,
+        amount: amount,
+        date: DateTime.now());
+
+  setState(() {
+    _userTransactions.add(newTx);
+  });
+  }
+
+  void _startAddNewTransaction(BuildContext ctx) {
+    showModalBottomSheet(
+        context: ctx,
+        builder: (_) {
+          return GestureDetector(
+            onTap: () {},
+            child: NewTransaction(_addNewTransaction),
+            behavior: HitTestBehavior.opaque
+            );
+        });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -32,7 +73,7 @@ class MyHomePage extends StatelessWidget {
         title: const Text('Expense Tracker'),
         actions: <Widget>[
           IconButton(
-            onPressed: () {},
+            onPressed: (() => _startAddNewTransaction(context)),
             icon: const Icon(Icons.add),
           ),
         ],
@@ -41,23 +82,21 @@ class MyHomePage extends StatelessWidget {
         child: Column(
           // mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: const <Widget>[
-            SizedBox(
+          children: <Widget>[
+            const SizedBox(
               width: double.infinity,
               child: Card(
                 color: Colors.amber,
                 child: Text('CHART!'),
               ),
             ),
-            UserTransactions()
+            TransactionList(_userTransactions)
           ],
         ),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      floatingActionButton: FloatingActionButton(onPressed: (() {
-        
-      }), child: const Icon(Icons.add)),
+      floatingActionButton: FloatingActionButton(
+          onPressed:(() => _startAddNewTransaction(context)), child: const Icon(Icons.add)),
     );
-    
   }
 }
