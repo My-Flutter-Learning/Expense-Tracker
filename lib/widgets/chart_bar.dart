29915661927c013397bf55579fd3aff8ef1586f9
victormutuku <victor.mutuku@strathmore.edu>
2022-03-11 11:58:40 +0300

    using the LayoutBuilder widget to get relative height.

--- lib/widgets/chart_bar.dart
+++ lib/widgets/chart_bar.dart
@@ -1,52 +1,64 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 class ChartBar extends StatelessWidget {
   final String label;
   final double spendingAmount;
   final double spendingPercentageOfTotal;
-  const ChartBar(this.label, this.spendingAmount, this.spendingPercentageOfTotal, {Key? key}) : super(key: key);
+  const ChartBar(
+      this.label, this.spendingAmount, this.spendingPercentageOfTotal,
+      {Key? key})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Column( children: [
-      SizedBox(
-        height: 20,
-        child: 
-          FittedBox(child: 
-            Text('\$${spendingAmount.toStringAsFixed(0)}')
-          ),
-      ),
-      const SizedBox(
-        height: 4,
-      ),
-      SizedBox(
-        height: 60,
-        width: 10,
-        child: Stack(
-          children: [
-            Container(
-              decoration: BoxDecoration(
-                border: Border.all(color: Colors.grey, width: 1.0),
-                color: const Color.fromRGBO(220, 220, 220, 1),
-                borderRadius: BorderRadius.circular(10),
-              ),
-            ),
-            FractionallySizedBox(
-              heightFactor: spendingPercentageOfTotal,
-              child: Container(
+    return LayoutBuilder(builder: (context, constraints){
+      return Column(
+      children: [
+        SizedBox(
+          height: constraints.maxHeight * 0.15,
+          child:
+              FittedBox(child: Text('\$${spendingAmount.toStringAsFixed(0)}')),
+        ),
+        SizedBox(
+          height: constraints.maxHeight * 0.05,
+        ),
+        SizedBox(
+          height: constraints.maxHeight * 0.6,
+          width: 10,
+          child: Stack(
+            children: [
+              Container(
                 decoration: BoxDecoration(
-                  color: Theme.of(context).primaryColor,
+                  border: Border.all(color: Colors.grey, width: 1.0),
+                  color: const Color.fromRGBO(220, 220, 220, 1),
                   borderRadius: BorderRadius.circular(10),
                 ),
               ),
-            )
-          ],
+              FractionallySizedBox(
+                heightFactor: spendingPercentageOfTotal,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).primaryColor,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
-      ),
-      const SizedBox(
-        height: 4,
-      ),
-      Text(label),
-    ],);
+        SizedBox(
+          height: constraints.maxHeight * 0.05,
+        ),
+        SizedBox(
+          height:  constraints.maxHeight * 0.15,
+          child: FittedBox(
+            child: Text(label)
+          )
+        ),
+      ],
+    );
+    },);
+    
   }
 }
