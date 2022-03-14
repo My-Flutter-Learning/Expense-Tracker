138423d9fd3ab94e3b2d574e115a82500441a1ef
victormutuku <victor.mutuku@strathmore.edu>
2022-03-14 11:17:04 +0300

    Respecting the Softkeyboard insets
    Allowed scrolling in the bottom modal sheet and also made the bottom padding to be dynamic (changes with change in device screen size)

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -50,44 +50,46 @@ class _NewTransactionState extends State<NewTransaction> {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 5,
-      child: Container(
-        padding: const EdgeInsets.all(10),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: <Widget>[
-            TextField(
-              decoration: (const InputDecoration(labelText: 'Title')),
-              controller: _titleController,
-              onSubmitted: (_) => _submitData(),
-            ),
-            TextField(
-              decoration: (const InputDecoration(labelText: 'Amount')),
-              controller: _amountController,
-              keyboardType: TextInputType.number,
-              onSubmitted: (_) => _submitData(),
-            ),
-            SizedBox(
-              height: 70,
-              child: Row(children: <Widget>[
-                Expanded(
-                    child: Text(_pickedDate == null
-                        ? 'No Date Chosen!'
-                        : 'Picked Date: ${DateFormat.yMMMd().format(_pickedDate!)}')),
-                TextButton(
-                    onPressed: _presentDatePicker,
-                    child: const Text(
-                      'Choose Date',
-                      style: TextStyle(fontWeight: FontWeight.bold),
-                    )),
-              ]),
-            ),
-            ElevatedButton(
-              onPressed: _submitData,
-              child: const Text('Add transaction'),
-            )
-          ],
+    return SingleChildScrollView(
+      child: Card(
+        elevation: 5,
+        child: Container(
+          padding: EdgeInsets.only(top: 10, left: 10, right: 10, bottom: MediaQuery.of(context).viewInsets.bottom + 10),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: <Widget>[
+              TextField(
+                decoration: (const InputDecoration(labelText: 'Title')),
+                controller: _titleController,
+                onSubmitted: (_) => _submitData(),
+              ),
+              TextField(
+                decoration: (const InputDecoration(labelText: 'Amount')),
+                controller: _amountController,
+                keyboardType: TextInputType.number,
+                onSubmitted: (_) => _submitData(),
+              ),
+              SizedBox(
+                height: 70,
+                child: Row(children: <Widget>[
+                  Expanded(
+                      child: Text(_pickedDate == null
+                          ? 'No Date Chosen!'
+                          : 'Picked Date: ${DateFormat.yMMMd().format(_pickedDate!)}')),
+                  TextButton(
+                      onPressed: _presentDatePicker,
+                      child: const Text(
+                        'Choose Date',
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      )),
+                ]),
+              ),
+              ElevatedButton(
+                onPressed: _submitData,
+                child: const Text('Add transaction'),
+              )
+            ],
+          ),
         ),
       ),
     );
