676c23d8cae814d90eb5233edb718f4b4ad0d0f8
victormutuku <victor.mutuku@strathmore.edu>
2022-03-18 16:38:08 +0300

    Keys and their use

--- lib/widgets/transaction_item.dart
+++ lib/widgets/transaction_item.dart
@@ -1,8 +1,10 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import '../models/transaction.dart';
 
-class TransactionItem extends StatelessWidget {
+class TransactionItem extends StatefulWidget {
   const TransactionItem({
     Key? key,
     required this.transaction,
@@ -13,44 +15,58 @@ class TransactionItem extends StatelessWidget {
   final Function deleteTx;
 
   @override
+  State<TransactionItem> createState() => _TransactionItemState();
+}
+
+class _TransactionItemState extends State<TransactionItem> with WidgetsBindingObserver{
+  late Color _bgColor;
+
+  @override
+  void initState() {
+    Color availableColors = Color.fromRGBO(Random().nextInt(256), Random().nextInt(256), Random().nextInt(256), 100);
+    _bgColor = availableColors;
+    super.initState();
+  }
+  
+  @override
   Widget build(BuildContext context) {
     return Card(
       elevation: 2,
       margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 5),
       child: ListTile(
         leading: CircleAvatar(
+          backgroundColor: _bgColor,
           radius: 30,
           child: Padding(
             padding: const EdgeInsets.all(6),
             child: FittedBox(
-                child: Text(
-                    '\$${transaction.amount.toStringAsFixed(2)}')),
+                child:
+                    Text('\$${widget.transaction.amount.toStringAsFixed(2)}')),
           ),
         ),
         title: Text(
-          '\$${transaction.title}',
+          widget.transaction.title,
           style: Theme.of(context).textTheme.bodyText1,
         ),
         subtitle: Text(
-          DateFormat.yMMMMd().format(transaction.date),
+          DateFormat.yMMMMd().format(widget.transaction.date),
         ),
         trailing: MediaQuery.of(context).size.width > 460
             ? TextButton.icon(
-                onPressed: () => deleteTx(transaction.id),
+                onPressed: () => widget.deleteTx(widget.transaction.id),
                 icon: Icon(
                   Icons.delete,
                   color: Theme.of(context).errorColor,
                 ),
                 label: Text(
                   'Delete',
-                  style:
-                      TextStyle(color: Theme.of(context).errorColor),
+                  style: TextStyle(color: Theme.of(context).errorColor),
                 ),
               )
             : IconButton(
                 icon: const Icon(Icons.delete),
                 color: Theme.of(context).errorColor,
-                onPressed: () => deleteTx(transaction.id)),
+                onPressed: () => widget.deleteTx(widget.transaction.id)),
       ),
     );
   }
--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -25,11 +25,8 @@ class TransactionList extends StatelessWidget {
               ],
             );
           })
-        : ListView.builder(
-            itemBuilder: (context, index) {
-              return TransactionItem(transaction: transactions[index], deleteTx: deleteTx);
-            },
-            itemCount: transactions.length,
+        : ListView(
+          children: transactions.map((tx) => TransactionItem( key: ValueKey(tx.id), transaction: tx, deleteTx: deleteTx)).toList()
           );
   }
 }
